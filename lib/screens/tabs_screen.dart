ac70c97a603d54feb854b14482c1b82818ca51d3
Amar <amarjaiswar1606@gmail.com>
2023-08-10 17:41:51 +0530

    added snackbar details in favorite item selection

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -4,7 +4,6 @@ import 'package:meals/screens/category_screen.dart';
 import 'package:meals/screens/meals_screen.dart';
 
 class TabsScreen extends StatefulWidget {
-
   const TabsScreen({super.key});
 
   @override
@@ -14,38 +13,53 @@ class TabsScreen extends StatefulWidget {
 }
 
 class _TabsScreenState extends State<TabsScreen> {
-
   int _selectedScreenIndex = 0;
-
   final List<Meal> _favoritesMeal = [];
 
-  void _toggleMealFavoritesStatus(Meal meal) {
+  void _showInfoMessage(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
 
+  void _toggleMealFavoritesStatus(Meal meal) {
     final isExisting = _favoritesMeal.contains(meal);
 
-    if(isExisting == true) {
-      _favoritesMeal.remove(meal);
-    }else {
-      _favoritesMeal.add(meal);
+    if (isExisting == true) {
+      setState(() {
+        _favoritesMeal.remove(meal);
+        _showInfoMessage('Meal is no longer a favorite.');
+      });
+    } else {
+      setState(() {
+        _favoritesMeal.add(meal);
+        _showInfoMessage('Marked as a favorite.');
+      });
     }
-
   }
 
   void _selectScreen(int index) {
     setState(() {
-       _selectedScreenIndex = index;
+      _selectedScreenIndex = index;
     });
-  } 
+  }
 
   @override
   Widget build(context) {
-
-    Widget activeScreen = CategoryScreen(onToggleFavorite: _toggleMealFavoritesStatus,);
+    Widget activeScreen = CategoryScreen(
+      onToggleFavorite: _toggleMealFavoritesStatus,
+    );
     var activeScreenTitle = 'Categories';
 
-    if(_selectedScreenIndex == 1) {
-       activeScreen =  MealsScreen(meals: [], onToggleFavorite: _toggleMealFavoritesStatus,);
-       activeScreenTitle = 'Your favorites';
+    if (_selectedScreenIndex == 1) {
+      activeScreen = MealsScreen(
+        meals: _favoritesMeal,
+        onToggleFavorite: _toggleMealFavoritesStatus,
+      );
+      activeScreenTitle = 'Your favorites';
     }
 
     return Scaffold(
@@ -56,8 +70,9 @@ class _TabsScreenState extends State<TabsScreen> {
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: _selectedScreenIndex,
         onTap: _selectScreen,
-        items: const  [
-          BottomNavigationBarItem(icon: Icon(Icons.set_meal), label: 'Categories'),
+        items: const [
+          BottomNavigationBarItem(
+              icon: Icon(Icons.set_meal), label: 'Categories'),
           BottomNavigationBarItem(icon: Icon(Icons.star), label: 'Favorites'),
         ],
       ),
